9432e908c4b62e7f1991c97e4442ae248905e207
DarkSunrise08 <88215000+DarkSunrise08@users.noreply.github.com>
2021-08-31 17:10:42 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var student
+
+function setup() {
+  createCanvas(400,400);
+
+  student = new Student(12,6,"john");
+  student.display();
+}
+
+
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   
+    constructor(age,grade,name){
+        this.age = age;
+        this.grade = grade;
+        this.name = name;
+    }
+
+    display(){
+        console.log(this.age+", "+this.grade+", "+this.name);
+    }
+
+}
+
+
+
